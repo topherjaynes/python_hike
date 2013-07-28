c791e92d1161f88fb9b51cc8e1e3a98b8a02f364
topherjaynes <chris.jaynes@gmail.com>
2013-07-28 14:49:27 -0400

    added reverse funtion and floor division

--- recursion.py
+++ recursion.py
@@ -1,6 +1,10 @@
 #recusrion practice
 import timeit
 
+'''
+Think of the base case!
+'''
+
 #countdown keeps calling it self till we hit the base case
 def countdown(n):
     #basecase
@@ -26,9 +30,8 @@ Calculate from the bottom
 This is o(n)
 '''
 def opt_fib(n):
-    result = [0,1]
     if n < 2:
-        return result[n]
+        return n
     else:
         fibNMinusOne = 1
         fibNMinusTwo = 0
@@ -38,12 +41,35 @@ def opt_fib(n):
             fibN = fibNMinusOne + fibNMinusTwo
             fibNMinusTwo = fibNMinusOne
             fibNMinusOne = fibN
-            print fibN
         return fibN
-            
+
+'''
+Implement recursive method reverse() that takes a nonnegative integer as 
+input and prints the digits of n vertically, starting with the low-order digit'''
+
+def reverse(n):
+    if n < 10:
+        print n
+    else:
+        #print last digit
+        print n%10
+        #remove last digit
+        reverse(n//10)
+'''
+this solution uses floor division, which I never heard of. Talked simliar with %
+>>> 123456//100
+1234
+>>> 123456%100
+56
+>>> t = 123456
+>>> print t//10,t%10
+12345 6
+>>>
+'''        
 
 
 if __name__ == '__main__':
     #countdown(10)
-    print fibonacci(10)
-    print opt_fib(10)
+    #print fibonacci(1)
+    #print opt_fib(1)
+    reverse(1234567)
