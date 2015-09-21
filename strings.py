32367c77d5f33690b1551b4d8c543d7a796aa439
topherjaynes <chris.jaynes@gmail.com>
2015-09-20 21:47:37 -0400

    trying to use some multiplication with the format function

--- strings.py
+++ strings.py
@@ -27,6 +27,23 @@ def main(argv):
 		for var in argv:
 			print "this is argument {count}: {var}".format(count=count,var =var)
 			count +=1
+	else:
+		"""
+		print stairs for 6x6 square
+		    #
+		   ##
+		  ###
+		 ####
+		#####
+		Using the format function and * chars to achieve
+		"""
+		
+		i=0
+		while i < 6:
+			sp = " "*(5-i)
+			h = "#"*i
+			print "{sp}{h}".format(sp=sp,h=h)
+			i+=1		
 
 
 if __name__ == '__main__':
